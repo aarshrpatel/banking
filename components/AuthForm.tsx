1fc919183037c79352fe08319e0635a653b19724
Aarsh Patel <aarshpatel2004@gmail.com>
2024-07-13 00:55:50 -0400

    Start Appwrite authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,6 +21,7 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type }: {type: string }) => {
   const router = useRouter();
@@ -45,18 +46,18 @@ const AuthForm = ({ type }: {type: string }) => {
     try {
         // Sign up with Appwrite & create plaid token
         if (type === 'sign-up') {
-            // const newUser = await signUp(data);
-            // setUser(newUser);
+            const newUser = await signUp(data);
+            setUser(newUser);
         }
         if (type === 'sign-in') {
-            // const response = await signIn({
-            //     email: data.email,
-            //     password: data.password
-            // })
+            const response = await signIn({
+                email: data.email,
+                password: data.password
+            })
 
-            // if (response) {
-            //     router.push('/')
-            // }
+            if (response) {
+                router.push('/')
+            }
         }
     } catch (error) {
         console.log(error)
